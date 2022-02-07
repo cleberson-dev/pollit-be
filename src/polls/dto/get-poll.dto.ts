cd967f9e7915869fa70afe23b40b5505cda45c9b
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2022-02-07 00:16:21 -0300

    Define fields for GetPollDto

---
+++ src/polls/dto/get-poll.dto.ts
@@ -0,0 +1,15 @@
+export class GetPollOptions {
+  id: string;
+  title: string;
+  votes: number;
+}
+
+export class GetPollDto {
+  id: string;
+  title: string;
+  expiresAt: string;
+  createdAt: string;
+  modifiedAt: string;
+  options: GetPollOptions[];
+  votes: number;
+}
